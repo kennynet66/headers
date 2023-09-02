3074b055a36486f18fbdb042ba6dbb887341416a
Kenny <kennynet66@gmail.com>
2023-09-02 12:55:16 +0300

    More prototypes

--- funcs.h
+++ funcs.h
@@ -7,5 +7,10 @@ void _putchar(char c);
 int _strcmp(char *s1, char *s2);
 int _strlen(char *s);
 char _strcat(char *dest, char *src);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+void _puts_recursion(char *s);
+void _puts_rev(char *s);
+int _strlen_recursion(char *s);
 
 #endif
