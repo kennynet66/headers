1ee40bfd48f06c76c9359a785d0bc683b165c27f
Kenny <kennynet66@gmail.com>
2023-09-02 12:48:54 +0300

    Added tests for functions

--- test.c
+++ test.c
@@ -15,8 +15,8 @@ int main(void)
 
     /* Test for _strcmp */
     printf("**********string comparison**********\n");
-    char str1[6] = "Hello";
-    char str2[6] = "World";
+    char str1[6] = "it";
+    char str2[6] = "works";
 
     printf("%d\n", _strcmp(str1, str2));
     printf("%d\n", _strcmp(str2, str1));
@@ -35,7 +35,30 @@ int main(void)
     printf("**********string length**********\n");
     char exmp[] = "My name is";
     int len = _strlen(exmp);
-    printf("%d", len);
+    printf("%d\n", len);
 
+    /* Test for factorial */
+    printf("**********factorial**********\n");
+    int n = 5;
+    int fact = factorial(n);
+    printf("%d", fact);
+
+    /* Test for _pow_recursion */
+    printf("**********power**********\n");
+    int x = 2;
+    int y = 3;
+    int pow = _pow_recursion(x, y);
+    printf("%d\n", pow);
+
+    /* Test for _puts_recursion */
+    printf("**********puts**********\n");
+    char *s = "It works";
+    _puts_recursion(s);
+
+    /* test for _puts_rev */
+    printf("**********puts_rev**********\n");
+    char *s1 = "It works";
+    _puts_rev(s1);
+    
     return (0);
 }
